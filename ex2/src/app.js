3d9134f7948345f33d3b5d729ad16d7e53b21056
mann13112003 <nguyennam13032003@gmail.com>
2025-08-12 14:39:56 +0700

    To Do App

---
+++ ex2/src/app.js
@@ -0,0 +1,100 @@
+const API_URL = "https://65d455c53f1ab8c63434e588.mockapi.io/job";
+
+const todoInput = document.querySelector(".todo__input");
+const addBtn = document.querySelector(".todo__add-btn");
+const todoList = document.querySelector(".todo__list");
+
+//render danh sach cac cong viec
+async function fetchTodos() {
+  try {
+    const res = await fetch(API_URL, { method: "GET" });
+    const todos = await res.json();
+    console.log(todos);
+    renderTodos(todos);
+  } catch (err) {
+    console.log(err);
+    alert("Loi tai danh sach cong viec");
+  }
+}
+function renderTodos(todos) {
+  const todoHtml = todos.map(
+    (todo) =>
+      `<li class = "todo__item">
+        <span class="todo__check ${
+          todo.checked ? "checked" : ""
+        }" data-index="${todo.id}">
+            <img src="../assets/svg/Vector.svg" alt="Checkbox" />
+        </span>
+        <span class="todo__content">${todo.content}</span>
+        <span class="todo__delete" data-index="${todo.id}">&times</span>
+      </li>`
+  );
+  todoList.innerHTML = todoHtml.join("");
+}
+//them cong viec moi
+async function handleAddTodo() {
+  const content = todoInput.value.trim();
+  if (!content) {
+    alert("Chua nhap noi dung cong viec!");
+    todoInput.focus();
+    return;
+  }
+
+  try {
+    await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        createdAt: new Date().toISOString(),
+        checked: false,
+        content,
+      }),
+    });
+    fetchTodos();
+    todoInput.value = "";
+    todoInput.focus();
+    alert("Them cong viec moi thanh cong");
+  } catch (err) {
+    console.error(err);
+    alert("Loi khi them cong viec moi");
+  }
+}
+addBtn.addEventListener("click", handleAddTodo);
+//xoa cong viec
+async function handledDeleteTodo(event) {
+  const deleteBtn = event.target.closest(".todo__delete");
+  if (!deleteBtn) return;
+  const id = deleteBtn.dataset.index;
+  const confirmDelete = confirm("Xac nhan xoa cong viec nay!");
+  if (!confirmDelete) return;
+  try {
+    await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+    await fetchTodos();
+    alert("Xoa cong viec thanh cong");
+  } catch (err) {
+    console.log(err);
+    alert("Loi xoa cong viec");
+  }
+}
+todoList.addEventListener("click", handledDeleteTodo);
+//cap nhat trang thai cong viec
+async function updateStatus(event) {
+  const checkbox = event.target.closest(".todo__check");
+  if (!checkbox) return;
+  try {
+    const id = checkbox.dataset.index;
+    const isChecked = !checkbox.classList.contains("checked");
+    await fetch(`${API_URL}/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ checked: isChecked }),
+    });
+    fetchTodos();
+  } catch (err) {
+    console.log(err);
+    alert("Loi cap nhat trang thai cong viec");
+  }
+}
+todoList.addEventListener("click", updateStatus);
+
+fetchTodos();
